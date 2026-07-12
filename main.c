cde806ba30e5cff689ced46adf467eab72dcaf64
Archie Healy <archiethehead@gmail.com>
2026-07-12 23:34:11 +0100

    Jump Table
    Feat: Implement a jump table comprised of 'command' structs, that reads
    user arguments and calls a function associated with a matched argument.

--- main.c
+++ main.c
@@ -1,9 +1,46 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <cjson/cjson.h>
 
-int main(int argc, char** argv[]) {
+typedef struct argcommandstruct {
 
-    printf("Hello World");
+    char* shortname;
+    char* longname;
+    void (*fptrs)(char* argv[]);
+
+} argcommand;
+
+
+void help(char* argv[]) {
+
+    printf("helped!");
+
+}
+
+const argcommand argcommands[] =    {
+                                    
+                                    {"-h","--help", &help},
+                                    {"test","testt", &help}
+                                    
+                                    };
+
+const int commandcount = sizeof(argcommands) / sizeof(argcommand);
+
+int main(int argc, char* argv[]) {
+
+    if (!argc) return 1;
+
+    for (int i = 0; i < commandcount; i++) {
+            
+        if (!(strcmp(argcommands[i].shortname, argv[1])) || !(strcmp(argcommands[i].longname, argv[1]))) {
+
+            argcommands[i].fptrs(argv);
+
+        }
+    
+    }
+
+    printf("hello world");
     return 0;
 
 }
